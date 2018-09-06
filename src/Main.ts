3eaf2b570425bd1bbba897c0857e7d19af3517dd
caozhichao <caozhichao@rongyi.com>
2018-09-06 15:04:04 +0800

    微信环境skins无法正确获取问题

--- src/Main.ts
+++ src/Main.ts
@@ -1,36 +1,20 @@
-//////////////////////////////////////////////////////////////////////////////////////
-//
-//  Copyright (c) 2014-present, Egret Technology.
-//  All rights reserved.
-//  Redistribution and use in source and binary forms, with or without
-//  modification, are permitted provided that the following conditions are met:
-//
-//     * Redistributions of source code must retain the above copyright
-//       notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above copyright
-//       notice, this list of conditions and the following disclaimer in the
-//       documentation and/or other materials provided with the distribution.
-//     * Neither the name of the Egret nor the
-//       names of its contributors may be used to endorse or promote products
-//       derived from this software without specific prior written permission.
-//
-//  THIS SOFTWARE IS PROVIDED BY EGRET AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
-//  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-//  IN NO EVENT SHALL EGRET AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;LOSS OF USE, DATA,
-//  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-//  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-//////////////////////////////////////////////////////////////////////////////////////
+
 egret.log('---------------------');
 egret.log(eg);
 egret.log('------------------------');
-
 class Main extends eg.EgMain {   
+
+    protected initBefore():void{
+		super.initBefore();
+        egret.log(window);
+        //微信h5调试环境中 window['__global'] != window
+        //导致挂载到__global中的skins 无法正常获取到 无法正常通过 getDefinitionByName 获取
+        // egret.log(window['__global'] == window);
+        if(window['__global'] != window){ 
+            window['__global'] = window;
+            // egret.log(window['__global']);
+        }
+    }
 
      protected async createGameScene(){
         window['fz'] = fz; //wx 需要主动挂载到window对象上
@@ -39,5 +23,8 @@ class Main extends eg.EgMain {
         // eg.UIManager.Instance.showUI(new test.PageTips()); 
         // eg.UIManager.Instance.showUI('test.PageTips'); 
         eg.UIManager.Instance.showUI('test.ParabolaTest');
+        // eg.log('---------------------------------------------------');
+        // eg.log(window['__global']);
+        egret.getDefinitionByName('skins.ButtonSkin');
     }
 }
