e4a2e8d9b42e437fa5c014bf372367b5ba86e268
caozhichao <caozhichao@rongyi.com>
2019-01-25 11:52:08 +0800

    界面展示

--- src/page/Number_sx.ts
+++ src/page/Number_sx.ts
@@ -6,13 +6,38 @@
 namespace sx{
 	export class Number_sx extends egret.Sprite{
 		private _mapVo:MapVo;
+		private _items:Item[][];
 		public constructor() {
 			super();
 			this.init();
 		}
 		public init():void{
 			this._mapVo = new MapVo();		
-			this._mapVo.find(0,0);	
+			// this._mapVo.find(0,0);	
+			this._items = [];
+			let itemList:Item[];
+			let item:Item;
+			for(let i:number = 0;i < MapVo.ROW; i++){
+				itemList = [];
+				for(let j:number = 0;j < MapVo.COL; j++){
+					item = new Item(this._mapVo.getNode(i,j));
+					item.x = j * Item.WIDTH;
+					item.y = i * Item.HEIGHT;
+					this.addChild(item);
+					itemList[j] = item;
+				}
+				this._items[i] = itemList;
+			}
+
+			let allList:Node[][] = this._mapVo.find(0,0);	
+			for(let i:number = 0; i < allList.length;i++){
+				let list:Node[] = allList[i];
+				for(let j:number = 0;j<list.length;j++){
+					let node:Node = list[j];
+					item = this._items[node.row][node.col];
+					item.alpha = 0.1;
+				}
+			}
 		}
 	}
 
@@ -188,6 +213,10 @@ namespace sx{
 			return this._data[row][col];
 		}
 
+		public get mapData():Node[][]{
+			return this._data;
+		}
+
 	}
 	class Node extends egret.HashObject{
 		public row:number;
@@ -202,5 +231,59 @@ namespace sx{
 		public toString():string{
 			return '[' + this. row + ']' + '[' + this.col + ']' + '=' + this.value;
 		}
+	}
+
+	class Item extends egret.Sprite{
+		private _bg:egret.Shape;
+		private _tf:eui.Label;
+		private _data:Node;
+		public static WIDTH:number = 64;
+		public static HEIGHT:number = 64;
+		public static COLORS:number[] = [0xff0000,
+											0xeb4310,
+											0xf6941d,
+											0xfbb417,
+											0xffff00,
+											0xcdd541,
+											0x99cc33,
+											0x3f9337,
+											0x219167,
+											0x239676,
+											0x24998d,
+											0x1f9baa,
+											0x0080ff,
+											0x3366cc,
+											0x333399,
+											0x003366,
+											0x800080,
+											0xa1488e,
+											0xc71585,
+											0xbd2158]
+		public constructor(data:Node){
+			super();
+			this._data = data;
+			this.initUI();
+		}		
+
+		public initUI():void{
+			this._bg = new egret.Shape();
+			this._tf = new eui.Label();
+			this._tf.width = Item.WIDTH;
+			this._tf.height = Item.HEIGHT;
+			this._tf.textAlign = egret.HorizontalAlign.CENTER;
+			this._tf.verticalAlign = egret.VerticalAlign.MIDDLE;
+			this.addChild(this._bg);
+			this.addChild(this._tf);
+
+			this.setData(this._data);
+		}
+		public setData(data:Node):void{
+			this._data = data;
+			this._bg.graphics.clear();
+			this._bg.graphics.beginFill(Item.COLORS[this._data.value-1]);
+			this._bg.graphics.drawRoundRect(0,0,Item.WIDTH,Item.HEIGHT,10,10);
+			this._bg.graphics.endFill();
+			this._tf.text = this._data.value + '';
+		}		
 	}
 }
