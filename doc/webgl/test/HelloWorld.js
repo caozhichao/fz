46e93965f6fac2a473ea6f424910935b7f39843f
caozhichao <caozhichao@rongyi.com>
2019-04-17 17:02:49 +0800

    屏幕坐标转3d坐标

--- doc/webgl/test/HelloWorld.js
+++ doc/webgl/test/HelloWorld.js
@@ -2,7 +2,7 @@ var canvas;
 var camera, scene, renderer;
 var raycaster;
 var mouse = new THREE.Vector2();
-
+var mesh;
 function main(){
     canvas = document.getElementById('demo');
     scene = new THREE.Scene();
@@ -12,12 +12,15 @@ function main(){
     camera = new THREE.OrthographicCamera(-2, 2, 1.5, -1.5, 1, 100);
     // var camera = new THREE.PerspectiveCamera(45, 400 / 300, 1, 10);
     // camera.position.set(3, 5, 5);
-    camera.position.set(0, 0, 5);
-    camera.lookAt(new THREE.Vector3(0,0,0));
+    camera.position.set(2, 0, 5);
+    // camera.rotateY(-15 * Math.PI / 180);
+    // camera.position.z = 5;
+    // camera.position.x = 3;
+    // camera.lookAt(new THREE.Vector3(0,0,0));
     scene.add(camera);
 
     var geometry = new THREE.CubeGeometry(1,1,1);
-    var mesh = new THREE.Mesh(geometry,
+    mesh = new THREE.Mesh(geometry,
         new THREE.MeshBasicMaterial({
             color: 0xff0000,
             wireframe: true,
@@ -43,9 +46,9 @@ function main(){
 
         
     }
-    var controls = new THREE.OrbitControls( camera );
-    controls.target.set( 0, 0, 0 );
-    controls.update();    
+    // var controls = new THREE.OrbitControls( camera );
+    // controls.target.set( 0, 0, 0 );
+    // controls.update();    
 
 
     document.addEventListener( 'click', onDocumentMouseMove );
@@ -54,10 +57,16 @@ function main(){
 
 function onDocumentMouseMove( event ) {
     event.preventDefault();
-    mouse.x = ( event.clientX / 400 ) * 2 - 1;
-    mouse.y = - ( event.clientY / 300) * 2 + 1;
+    mouse.x = ( (event.clientX-8) / 400 ) * 2 - 1;
+    mouse.y = - ( (event.clientY-8) / 300) * 2 + 1;
 
-    raycaster.setFromCamera( mouse, camera );
-        var intersects = raycaster.intersectObjects( scene.children,true);
-        console.log(intersects.length,event);
+    // raycaster.setFromCamera( mouse, camera );
+    // var intersects = raycaster.intersectObjects( scene.children,true);
+    // console.log(intersects.length,event);
+
+    let p = new THREE.Vector3(mouse.x, mouse.y, 0).unproject(camera);    
+
+    mesh.position.set(p.x,p.y,p.z);
+
+    console.log(p);
 }
