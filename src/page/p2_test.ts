623e64cf30809bb7f71d29a627a30caf543ecc4e
caozhichao <caozhichao@rongyi.com>
2019-05-09 11:47:02 +0800

    p2

---
+++ src/page/p2_test.ts
@@ -0,0 +1,187 @@
+module test {
+	export class p2_test extends egret.Sprite{
+
+		//debug模式，使用图形绘制
+    	private isDebug: boolean = true;
+
+		public constructor() {
+			super();
+
+			
+
+			var factor: number = 50;
+
+			//创建world
+			var world: p2.World = new p2.World();
+			world.sleepMode = p2.World.BODY_SLEEPING;
+
+			//创建plane
+			var planeShape: p2.Plane = new p2.Plane();
+			var planeBody: p2.Body = new p2.Body();
+			planeBody.addShape(planeShape);
+			planeBody.displays = [];
+			world.addBody(planeBody);
+
+			//添加一个矩形
+
+			let p2Pos = this.toP2Position(300,300);
+
+			
+
+
+			var boxShape: p2.Shape = new p2.Box({width: 2, height: 1});
+			var boxBody: p2.Body = new p2.Body({ mass: 1, position: [p2Pos[0], p2Pos[1]], angularVelocity: 0 ,fixedY :true});
+			boxBody.addShape(boxShape);
+
+			let display = this.createBox((<p2.Box>boxShape).width * factor,(<p2.Box>boxShape).height * factor);
+			display.anchorOffsetX = display.width / 2;
+			display.anchorOffsetY = display.height / 2;
+			this.addChild(display);
+
+			boxBody.displays = [display];
+			world.addBody(boxBody);
+
+
+			egret.Ticker.getInstance().register(function(dt) {
+				if (dt < 10) {
+					return;
+				}
+				if (dt > 1000) {
+					return;
+				}
+				world.step(dt / 1000);
+
+				var stageHeight: number = egret.MainContext.instance.stage.stageHeight;
+				var l = world.bodies.length;
+				for (var i: number = 0; i < l; i++) {
+					var boxBody: p2.Body = world.bodies[i];
+					// console.log(boxBody.position);
+					var box: egret.DisplayObject = boxBody.displays[0];
+					if (box) {
+						box.x = boxBody.position[0] * factor;
+						box.y = stageHeight - boxBody.position[1] * factor;
+						box.rotation = 360 - (boxBody.angle + boxBody.shapes[0].angle) * 180 / Math.PI;
+						if (boxBody.sleepState == p2.Body.SLEEPING) {
+							box.alpha = 0.5;
+						}
+						else {
+							box.alpha = 1;
+						}
+					}
+				}
+			}, this);
+
+
+			//添加鼠标约束
+
+
+			let _stage = eg.Config.stage;
+			_stage.addEventListener(egret.TouchEvent.TOUCH_BEGIN,(evt:egret.TouchEvent)=>{
+				let p2Pos = this.toP2Position(evt.stageX,evt.stageY);
+				var hitBodies = world.hitTest(p2Pos, [boxBody],10);
+				console.log(hitBodies);
+
+			},this);
+
+
+
+
+
+			//鼠标点击添加刚体
+        // eg.Config.stage.addEventListener(egret.TouchEvent.TOUCH_BEGIN, addOneBox, this);
+        var self = this;
+
+			function addOneBox(e: egret.TouchEvent): void {
+				var positionX: number = Math.floor(e.stageX / factor);
+				var positionY: number = Math.floor((egret.MainContext.instance.stage.stageHeight - e.stageY) / factor);
+				var display: egret.DisplayObject;
+				if (Math.random() > 0.5) {
+					//添加方形刚体
+					//var boxShape: p2.Shape = new p2.Rectangle(2, 1);
+					var boxShape: p2.Shape = new p2.Box({width: 2, height: 1});
+					var boxBody: p2.Body = new p2.Body({ mass: 1, position: [positionX, positionY], angularVelocity: 1 });
+					boxBody.addShape(boxShape);
+					world.addBody(boxBody);
+
+					if(self.isDebug){
+					display = self.createBox((<p2.Box>boxShape).width * factor,(<p2.Box>boxShape).height * factor);
+					}else{
+						display = self.createBitmapByName("rect");
+					}
+					display.width = (<p2.Box>boxShape).width * factor;
+					display.height = (<p2.Box>boxShape).height * factor;
+				}
+				else {
+					//添加圆形刚体
+					//var boxShape: p2.Shape = new p2.Circle(1);
+					var boxShape: p2.Shape = new p2.Circle({ radius: 1 });
+					var boxBody: p2.Body = new p2.Body({ mass: 1, position: [positionX, positionY] });
+					boxBody.addShape(boxShape);
+					world.addBody(boxBody);
+
+					if (self.isDebug) {
+						display = self.createBall((<p2.Circle>boxShape).radius*factor);
+					} else {
+						display = self.createBitmapByName("circle");
+					}
+
+					display.width = (<p2.Circle>boxShape).radius * 2 * factor;
+					display.height = (<p2.Circle>boxShape).radius * 2 * factor;
+				}
+
+				display.anchorOffsetX = display.width / 2;
+				display.anchorOffsetY = display.height / 2;
+
+				boxBody.displays = [display];
+				self.addChild(display);
+			}
+
+		}
+
+			/**
+		 * 根据name关键字创建一个Bitmap对象。name属性请参考resources/resource.json配置文件的内容。
+		 */
+		private createBitmapByName(name: string): egret.Bitmap {
+			var result: egret.Bitmap = new egret.Bitmap();
+			var texture: egret.Texture = RES.getRes(name);
+			result.texture = texture;
+			return result;
+		}
+		/**
+		 * 创建一个圆形
+		 */
+		private createBall(r: number): egret.Shape {
+			var shape = new egret.Shape();
+			shape.graphics.beginFill(0xfff000);
+			shape.graphics.drawCircle(r, r, r);
+			shape.graphics.endFill();
+			return shape;
+		}
+		/**
+		 * 创建一个方形
+		 */
+		private createBox(width:number,height:number): egret.Shape {
+			var shape = new egret.Shape();
+			shape.graphics.beginFill(0xfff000);
+			shape.graphics.drawRect(0,0,width,height);
+			shape.graphics.endFill();
+			return shape;
+		}
+
+		/**
+		 * @param x  stageX
+		 * @param y  stageY
+		 * egret 坐标转换到p2坐标
+		 */
+		public toP2Position(x:number,y:number):number[]{
+			var positionX: number = Math.floor(x / 50);
+			var positionY: number = Math.floor((egret.MainContext.instance.stage.stageHeight - y) / 50);
+			console.log(positionX + '|' + positionY);
+			return [positionX,positionY];
+
+			// x = (x - w / 2) / 50;
+            // y = (y - h / 2) / -50;
+
+		}
+	}
+}
